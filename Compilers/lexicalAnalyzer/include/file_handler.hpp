9adcaf775b8ff692cfe656ed560aca1dd8d53c23
Solkaz <jheld135@gmail.com>
2016-09-06 12:15:47 -0400

    Initial commit

---
+++ Compilers/lexicalAnalyzer/include/file_handler.hpp
@@ -0,0 +1,16 @@
+#ifndef FILE_HANDLER_HPP_
+#define FILE_HANDLER_HPP_
+#include <iostream>
+#include <fstream>
+#include <string>
+
+class FileHandler {
+public:
+    FileHandler();
+    FileHandler(std::string&);
+    void LoadFile(std::string&);
+private:
+    
+};
+
+#endif // FILE_HANDLER_HPP_
